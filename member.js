0b8fb3d0eaa5e3654798e0d64e97b661cdb243af
line-313 <88546383+line-313@users.noreply.github.com>
2023-11-27 04:16:18 +0000

    Copilot second commit!

---
+++ member.js
@@ -0,0 +1,24 @@
+function skillsMember() {
+  return [
+    {
+      name: 'Kendall',
+      skills: ['JavaScript', 'React', 'Node.js']
+    },
+    {
+      name: 'Samantha',
+      skills: ['JavaScript', 'React', 'Node.js', 'CSS', 'HTML']
+    },
+    {
+      name: 'George',
+      skills: ['Go', 'Python', 'JavaScript']
+    },
+    {
+      name: 'John',
+      skills: ['Python', 'JavaScript']
+    },
+    {
+      name: 'Susan',
+      skills: ['JavaScript', 'React', 'Node.js', 'CSS']
+    }
+  ];
+}
